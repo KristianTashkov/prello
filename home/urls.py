ae50ed3cec5e987db8d304e6532225e18cb9533a
Kristian Tashkov <kristian.tashkov@gmail.com>
2015-07-10 15:14:40 +0300

    Register users form

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^login/$', views.login_request, name="login"),
     url(r'^logout/$', views.logout_request, name="logout"),
+    url(r'^register/$', views.register_request, name="register"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
+
 def index(request):
-	return render(request, 'home/register.html');
+    if request.user.is_active:
+        return redirect("boards")
+    return render(request, 'home/register.html')
+
 
 def login_request(request):
     username = request.POST['login_username']
@@ -15,8 +20,34 @@ def login_request(request):
             login(request, user)
             return redirect("boards")
     else:
-        return render(request, 'home/register.html', { 'error_message': "Invalid login details"})
+        return render(request, 'home/register.html',
+                      {'error_message': "Invalid login details"})
+
 
 def logout_request(request):
     logout(request)
     return redirect('/')
+
+
+def register_request(request):
+    username = request.POST['username']
+    email = request.POST['username']
+    password1 = request.POST['password1']
+    password2 = request.POST['password2']
+
+    user, new = User.objects.get_or_create(username=username)
+    if not new:
+        return render(request, 'home/register.html',
+                      {'error_message': "Username already exists"})
+
+    if password1 != password2:
+        return render(request, 'home/register.html',
+                      {'error_message':
+                       "Passwords don't match, please try again."})
+
+    user.email = email
+    user.set_password(password1)
+    user.save()
+    user = authenticate(username=username, password=password1)
+    login(request, user)
+    return redirect("boards")
